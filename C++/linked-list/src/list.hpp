95f7cae2f14eb847ec45eb62848767abf2d0a7e6
projektir <oprojektir@gmail.com>
2017-10-26 16:30:17 -0700

    Implement a linked list

--- C++/linked-list/src/list.hpp
+++ C++/linked-list/src/list.hpp
@@ -1,6 +1,21 @@
+#include <iostream>
+
+namespace ads {
+
 class List {
     struct Node {
         int data;
         Node* next;
     };
+
+    Node* head;
+public:
+    List();
+    ~List();
+
+    void insert(const int data);
+
+    friend std::ostream& operator<<(std::ostream& os, const List& list);
+};
+
 }
--- C++/linked-list/src/main.cpp
+++ C++/linked-list/src/main.cpp
@@ -1,3 +1,20 @@
+#include <iostream>
+
+#include "list.hpp"
+
+using ads::List;
+
 int main() {
+    List list;
+    list.insert(3);
+    list.insert(5);
+    list.insert(15);
+
+    std::cout << list << std::endl;
+
+    int blar;
+    std::cin >> blar;
+    std::cin >> blar;
+
     return 0;
 }
