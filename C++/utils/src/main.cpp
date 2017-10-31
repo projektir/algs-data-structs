9d54159c9751212cad95507996291ced6a87f603
projektir <oprojektir@gmail.com>
2017-10-31 14:38:50 -0700

    more utils

--- C++/utils/src/main.cpp
+++ C++/utils/src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <regex>
+
+void regex_test(const std::string& source) {
+    //"(2,3), (4, 5), (9,10)"
+    //std::regex re("\\([0-9],[0-0]\\)");
+
+    //std::string s1("{1,2,3}{4,5,6}{7,8,9}");
+    //std::regex e(R"~((\d+),(\d+),(\d+))~");
+
+    // Match numbers
+    std::regex re("\d+");
+    std::sregex_iterator iter(source.begin(), source.end(), re);
+    std::sregex_iterator end;
+
+    std::vector<int> ints;
+
+    while (iter != end)
+    {
+        for (int i = 1; i < iter->size(); i++) {
+            ints.push_back(std::stoi((*iter)[i]));
+        }       
+
+        iter++;
+    }
+}
 
 std::vector<std::string> split(const std::string& source, const std::string& delimiter) {
     auto source_copy = source;
@@ -29,8 +54,11 @@ int main() {
     std::string line;
     
     while (std::getline(std::cin, line)) {
-        auto strings = split(line, " ");
-        strings.erase(strings.begin() + 3);
+        auto strings = split(line, ", ");
+        //strings.erase(strings.begin() + 3);
+
+        regex_test(line);
+
         int blar = 3;
     }
 
